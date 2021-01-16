11cedc61958345462689f689446eab6cab0b0530
grigory <gasparovapp@icloud.com>
2021-01-16 16:34:06 +0400

    proba

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Петр","Петров", (byte) 44);
+        userService.saveUser("Иван","Иванов", (byte) 34);
+        userService.saveUser("Сергей","Сергеев", (byte) 18);
+        userService.saveUser("Аристарх","Аристархов", (byte) 99);
+        List<User> results = userService.getAllUsers();
+        results.forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,23 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLSyntaxErrorException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    private final Connection connection = Util.getConnection();
+
+
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,7 +25,14 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE Users (Id bigint PRIMARY KEY AUTO_INCREMENT, Name varchar(255), LastName varchar(255), Age INT)");
+        }catch (SQLSyntaxErrorException sqlSyntaxErrorException){
+            System.out.println("Table already exists");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -22,7 +42,20 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
 
+            transaction = session.beginTransaction();
+
+            session.save(new User());
+
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
